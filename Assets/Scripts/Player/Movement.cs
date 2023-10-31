40f2d891c6297f1cc2d0f8aa40ec311bbf95257d
Oscar Torres <oscar.torres.m@uni.pe>
2023-10-31 17:16:12 -0500

    animations and little script

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Player
+{
+    public class Movement : MonoBehaviour
     {
         
+        [SerializeField] private int velocity = 100;
+        
+
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
     }
 }
+
