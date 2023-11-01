29655354d7a767a8bd8f981d264f5d1420dd306d
Oscar Torres <oscar.torres.m@uni.pe>
2023-10-31 19:32:01 -0500

    PlayerMovement script improvement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,55 +1,54 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float timeToStop;
-    [SerializeField]
-    private float speed;
-    private float xAxis;
-    private float yAxis;
-    private Vector2 inputVector;
-    private Vector2 movementVector;
-    private Vector2 currentVelocity;
-    private Rigidbody2D rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
 
-    void Update()
-    {
-        GetInput();
-        Aim();
-    }
-    private void FixedUpdate()
-    {
-        movementVector = Vector2.SmoothDamp(movementVector, inputVector, ref currentVelocity, timeToStop);
-        Move();
-    }
+	[SerializeField] private float acelerationTime;
+	[SerializeField] private float speed;
 
-    private void Move()
-    {
-        rb.velocity = movementVector * speed;
-    }
+	private Vector2 currentVelocity;
+	private Rigidbody2D rb;
+	Vector2 movementVector;
 
-    private void GetInput()
-    {
-        xAxis = Input.GetAxisRaw("Horizontal");
-        yAxis = Input.GetAxisRaw("Vertical");
 
-        inputVector = new Vector2(xAxis, yAxis).normalized;
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 
-    private void Aim()
-    {
-        Vector2 mousePosition = Input.mousePosition;
-        Vector3 distance = transform.position - Camera.main.ScreenToWorldPoint(mousePosition);
-        float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle+90);
-    }
-    
+	void Update()
+	{
+
+	}
+	
+	private void FixedUpdate()
+	{	
+		Aim();
+		Move(GetInput());
+	}
+
+	private void Move(Vector2 target)
+	{
+		movementVector = Vector2.SmoothDamp(movementVector, target, ref currentVelocity, acelerationTime);
+
+		rb.velocity = movementVector * speed;
+	}
+
+	private void Aim()
+	{
+		Vector2 mousePosition = Input.mousePosition;
+
+		Vector3 distance = transform.position - Camera.main.ScreenToWorldPoint(mousePosition);
+		float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+
+		transform.rotation = Quaternion.Euler(0, 0, angle+90);
+	}
+
+	private Vector2  GetInput()
+	{
+		float xAxis = Input.GetAxisRaw("Horizontal");
+		float yAxis = Input.GetAxisRaw("Vertical");
+
+		return new Vector2(xAxis, yAxis).normalized;
+	}
 }
