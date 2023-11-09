ed00116a8f7a80453f441abb753b1b3207f44954
SandroCJ210 <sandro.carrillo.j@uni.pe>
2023-11-09 09:09:34 -0500

    Pick Up Weaponsv1

---
+++ Assets/Scripts/PickUpWeapons.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class PickUpWeapons : MonoBehaviour
+{
+    [SerializeField] private Player player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Handgun"))
+        {
+            player.WeaponType = WeaponType.Handgun;
+        }
+        else if (collision.transform.CompareTag("Shotgun"))
+        {
+            player.WeaponType = WeaponType.Shotgun;
+        }
+        else if (collision.transform.CompareTag("Automatic"))
+        {
+            player.WeaponType = WeaponType.Automatic;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,11 +17,18 @@ public class Player : MonoBehaviour
 	[SerializeField] private Transform firePivot;
 	[SerializeField] private GameObject bulletPrefab;
 	[SerializeField] private float timeBetweenShots;
+	[SerializeField] private float automaticFireRate;
 	[SerializeField] private bool shootingInDelay;
-	[SerializeField] private WeaponType weaponType;
 	[SerializeField] private int dispersionAngle;
+	[SerializeField] private int numBullets;
+	[SerializeField] private WeaponType weaponType;
+	
 	private float nextTimeToShot;
 	private bool isShooting;
+	private float angleIncrement;
+	private Vector3 adjustVectorX;
+	private Vector3 adjustVectorY;
+	public int ammo;
 
 	private Vector2 currentVelocity;
 	private Vector2 movementVector;
@@ -29,11 +36,33 @@ public class Player : MonoBehaviour
 
 	private Rigidbody2D rb;
 	private Animator animator;
-	
+
+	public WeaponType WeaponType
+	{
+		get => weaponType;
+		set
+		{
+			weaponType = value;
+			if (value == WeaponType.Automatic)
+			{
+				ammo = 24;
+			}
+			else if (value == WeaponType.Shotgun)
+			{
+				ammo = 6;
+			}
+			else if (value == WeaponType.Handgun)
+			{
+				ammo = 8;
+			}
+		}
+	}
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
 		animator = GetComponentInChildren<Animator>();
+		angleIncrement = dispersionAngle / numBullets*1.0f;
 	}
 
 	void Update()
@@ -61,7 +90,6 @@ public class Player : MonoBehaviour
 		Vector2 mousePosition = Input.mousePosition;
 		Vector2 distance = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
 		float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
-
 		transform.rotation = Quaternion.Euler(0, 0, angle-90);
 	}
 
@@ -75,10 +103,10 @@ public class Player : MonoBehaviour
 
 	private void GetAttackInput()
     {
-		if( weaponType != WeaponType.Automatic)
+		if( WeaponType != WeaponType.Automatic)
         {
 			isShooting = Input.GetMouseButtonDown(0);
-			if (Input.GetMouseButtonDown(0))
+			if (Input.GetMouseButtonDown(0) && ammo >0)
 			{
 				shootingInDelay = true;
 			}
@@ -88,31 +116,56 @@ public class Player : MonoBehaviour
 			isShooting = Input.GetMouseButton(0);
 		}
     }
+
 	private void TryShoot()
 	{
-		if(weaponType == WeaponType.Handgun)
+		if(WeaponType == WeaponType.Handgun)
         {
 			if (isShooting || shootingInDelay)
 			{
-				if (Time.time >= nextTimeToShot)
+				if (Time.time >= nextTimeToShot && ammo > 0)
 				{
 					nextTimeToShot = Time.time + timeBetweenShots;
 					Instantiate(bulletPrefab, firePivot.transform.position, firePivot.transform.rotation);
 					shootingInDelay = false;
+					ammo--;
 				}
 			}
 		}
-		else if(weaponType == WeaponType.Shotgun)
+		else if(WeaponType == WeaponType.Shotgun)
         {
 			if (isShooting || shootingInDelay)
 			{
-				if (Time.time >= nextTimeToShot)
+				if (Time.time >= nextTimeToShot && ammo > 0)
 				{
 					nextTimeToShot = Time.time + timeBetweenShots;
-					Instantiate(bulletPrefab, firePivot.transform.position, firePivot.transform.rotation * Quaternion.Euler(0,0, dispersionAngle));
+					for(int i = 0; i < numBullets/2; i++)
+                    {
+						adjustVectorX = firePivot.transform.right * Mathf.Sin(Mathf.Deg2Rad * angleIncrement*(i+1)) * bulletPrefab.transform.localScale.y;
+						adjustVectorY = firePivot.transform.up * (bulletPrefab.transform.localScale.y - Mathf.Cos(Mathf.Deg2Rad * angleIncrement * (i + 1)) * bulletPrefab.transform.localScale.y);
+						Instantiate(bulletPrefab, firePivot.transform.position - adjustVectorY - adjustVectorX, firePivot.transform.rotation * Quaternion.Euler(0, 0, angleIncrement * (i + 1)));
+					}
 					Instantiate(bulletPrefab, firePivot.transform.position, firePivot.transform.rotation);
-					Instantiate(bulletPrefab, firePivot.transform.position, firePivot.transform.rotation * Quaternion.Euler(0,0, dispersionAngle*-1));
+					for (int i = 0; i < numBullets/2; i++)
+                    {
+						adjustVectorX = firePivot.transform.right * Mathf.Sin(Mathf.Deg2Rad * angleIncrement*(i+1)) * bulletPrefab.transform.localScale.y;
+						adjustVectorY = firePivot.transform.up * (bulletPrefab.transform.localScale.y - Mathf.Cos(Mathf.Deg2Rad * angleIncrement * (i + 1)) * bulletPrefab.transform.localScale.y);
+						Instantiate(bulletPrefab, firePivot.transform.position - adjustVectorY + adjustVectorX, firePivot.transform.rotation * Quaternion.Euler(0, 0, angleIncrement * -1 *(i + 1)));
+					}
 					shootingInDelay = false;
+					ammo--;
+				}
+			}
+		}
+		else if (WeaponType == WeaponType.Automatic)
+        {
+			if (isShooting)
+			{
+				if (Time.time >= nextTimeToShot && ammo > 0)
+				{
+					nextTimeToShot = Time.time + automaticFireRate;
+					Instantiate(bulletPrefab, firePivot.transform.position, firePivot.transform.rotation);
+					ammo--;
 				}
 			}
 		}
